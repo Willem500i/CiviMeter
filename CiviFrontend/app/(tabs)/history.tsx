0bbc1e9eba769e6e49deb75f3c2bc068033466e8
Willem Neuefeind Lessig <willemnl303@gmail.com>
2025-01-25 12:54:31 -0500

    basic app created

---
+++ CiviFrontend/app/(tabs)/history.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function HistoryScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>History Screen</Text>
+      {/* Here you'll display the user’s submitted tickets or scanning history */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
