c043a937a73f17c7262feffecbbe6c3e0f8a021c
Willem Neuefeind Lessig <willemnl303@gmail.com>
2025-01-26 03:59:15 -0500

    fixed crashing

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -11,6 +11,7 @@ import AntDesign from "@expo/vector-icons/AntDesign";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import Feather from "@expo/vector-icons/Feather";
+import "react-native-reanimated";
 
 import styles from "../styles";
 
